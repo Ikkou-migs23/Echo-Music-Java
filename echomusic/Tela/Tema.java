499ae175182cd5d06a5249959808d7de8107864f
heitorssr <heitorssr7@gmail.com>
2026-09-22 14:47:47 -0300

    Delete echomusic directory

--- echomusic/Tela/Tema.java
+++
@@ -1,8 +0,0 @@
-package Tela;
-
-import java.awt.Color;
-
-public class Tema {
-	public static final Color ROXO = new Color(105, 65, 180);
-	public static final Color VERMELHO = new Color(224, 62, 76);
-}
